f0c4c05feca5522edf57f6fa5e907227341be721
vkopio <ville.kopio@gmail.com>
2019-07-26 09:52:30 +0300

    Added check that DB_POD variable is set before continuing and exported some helper variables

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+if [ -z "$DB_POD" ]; then
+    echo "Environment variable DB_POD must be set, exiting"
+    exit 1
+fi
+
+export BACKUP_FILE="${POSTGRES_DB}_backup.sql.gz"
+export BACKUP="${POSTGRES_DB}/${BACKUP_FILE}"
 
 # Ensure there is a directory for the backup
 mkdir -p /app/${POSTGRES_DB}
