2fe6f87dcd58219adb1525d1564dc9b55df68c44
vkopio <ville.kopio@gmail.com>
2019-07-26 09:31:13 +0300

    Renamed auth script to init and added backup dir creation

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+
+# Ensure there is a directory for the backup
+mkdir -p /app/${POSTGRES_DB}
+
 gcloud auth activate-service-account --key-file=${GOOGLE_APPLICATION_CREDENTIALS}
 gcloud container clusters get-credentials satakuntatalo-cluster
